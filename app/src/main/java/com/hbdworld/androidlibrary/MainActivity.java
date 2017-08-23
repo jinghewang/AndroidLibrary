d7d1ca1f3c7002e27d8ad99b932a40a829ac0747
jinghewang <jinghewang@163.com>
2017-08-23 21:22:48 +0800

    add layout

--- app/src/main/java/com/hbdworld/androidlibrary/MainActivity.java
+++ app/src/main/java/com/hbdworld/androidlibrary/MainActivity.java
@@ -1,13 +1,85 @@
 package com.hbdworld.androidlibrary;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.zxing.WriterException;
+import com.google.zxing.encoding.EncodingHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //客户主扫
+
+    private EditText resultTv;
+
+    private EditText authCode;
+
+    private EditText payCode;
+
+    private WebView wb;
+
+    private RadioGroup group_temo;
+    private RadioButton checkRadioButton;
+
+    private ImageView QrCode;
+
+    private static final String tag=MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        //
+        authCode = (EditText)findViewById(R.id.authCode);
+        payCode = (EditText) findViewById(R.id.payCode);
     }
+
+    /**
+     * 点击事件
+     * @param view
+     */
+    public void eventScan(View view) {
+        //扫描成功后调用
+        Bundle bundle = new Bundle();
+        Intent intent = new Intent(MainActivity.this,ScanActivity.class);
+        startActivityForResult(intent,1,bundle);
+    }
+
+    /**
+     * 生成二维码
+     * @param view
+     */
+    public void eventCreate(View view) {
+        //扫描成功后调用
+        try {
+            //获取输入的文本信息
+            String str = payCode.getText().toString().trim();
+            if(str != null && !"".equals(str.trim())){
+                //根据输入的文本生成对应的二维码并且显示出来
+                Bitmap mBitmap = EncodingHandler.createQRCode(str.toString(), 800);
+                if(mBitmap != null){
+                    Toast.makeText(this,"二维码生成成功！",Toast.LENGTH_SHORT).show();
+                    QrCode.setImageBitmap(mBitmap);
+                }
+            }else{
+                Toast.makeText(this,"文本信息不能为空！",Toast.LENGTH_SHORT).show();
+            }
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/hbdworld/androidlibrary/ScanActivity.java
@@ -0,0 +1,88 @@
+package com.hbdworld.androidlibrary;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.rbj.zxing.decode.QrcodeDecode;
+
+public class ScanActivity extends Activity {
+
+
+	private SurfaceView scanPreview;//相机
+	private RelativeLayout scanCropView;//扫描框
+	private ImageView scanLine;//扫描框中间的线
+	
+	private QrcodeDecode qd;
+
+	private String Tag = "HBD--";
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+
+		Window window = getWindow();
+		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.activity_capture);
+		scanPreview = (SurfaceView) findViewById(R.id.capture_preview);
+		scanCropView = (RelativeLayout) findViewById(R.id.capture_crop_view);
+		scanLine = (ImageView) findViewById(R.id.capture_scan_line);
+
+		qd = new QrcodeDecode(this,scanPreview,scanCropView) {
+			
+			@Override
+			public void handleDecode(Bundle bundle) {
+				//扫描成功后调用
+				//startActivity(new Intent(ScanActivity.this, ResultActivity.class).putExtras(bundle));
+
+				String result = bundle.getString(QrcodeDecode.BARCODE_RESULT);
+				Log.e(Tag, result);
+				//Toast.makeText(ScanActivity.this,result,Toast.LENGTH_LONG);
+				Intent intent = new Intent();
+				Bundle b = new Bundle();
+				b.putString("result",result);
+				intent.putExtras(b);
+				this.setResult(0,intent);
+
+				qd.finish();
+			}
+		};
+
+		//animation
+		TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT,				0.9f);
+		animation.setDuration(4500);
+		animation.setRepeatCount(-1);
+		animation.setRepeatMode(Animation.RESTART);
+		scanLine.startAnimation(animation);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//在此处开起扫描
+		qd.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		//
+		qd.onPause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		//释放资源
+		qd.onDestroy();
+		super.onDestroy();
+	}
+}
